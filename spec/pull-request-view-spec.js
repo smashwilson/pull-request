b14d2b9e8e53a31e8cee738a02e868d6766e8df6
Ash Wilson <smashwilson@gmail.com>
2015-09-01 08:41:53 -0400

    Generated package, decaffeinated.

---
+++ spec/pull-request-view-spec.js
@@ -0,0 +1,7 @@
+var PullRequestView = require('../lib/pull-request-view');
+
+describe("PullRequestView", function() {
+  return it("has one valid test", function() {
+    return expect("life").toBe("easy");
+  });
+});
