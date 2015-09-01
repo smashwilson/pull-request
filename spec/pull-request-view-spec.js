e63aeba6ef010711430d77ada2f2b9ec62b7cb01
Ash Wilson <smashwilson@gmail.com>
2015-09-01 09:06:23 -0400

    Delete generated specs.

--- spec/pull-request-view-spec.js
+++
@@ -1,9 +0,0 @@
-"use babel";
-
-var PullRequestView = require('../lib/pull-request-view');
-
-describe("PullRequestView", function() {
-  return it("has one valid test", function() {
-    return expect("life").toBe("easy");
-  });
-});
