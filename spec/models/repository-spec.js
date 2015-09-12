3801548d4cfc638e71b6ac6a8b6895b4179958d0
Ash Wilson <smashwilson@gmail.com>
2015-09-12 11:55:18 -0500

    Fetch the current pull request.

--- spec/models/repository-spec.js
+++ spec/models/repository-spec.js
@@ -1,6 +1,7 @@
 "use babel";
 
 import Repository from '../../lib/models/repository';
+import PullRequest, {State} from '../../lib/models/pull-request';
 
 import demoTransport from '../../lib/transport/demo';
 
@@ -69,6 +70,61 @@ describe("Repository", () => {
         expect(err.message).toBe("git@elsewhere.horse/huh.git doesn't look like a GitHub repository");
       });
     });
+
+  });
+
+  describe("currentPullRequest", () => {
+
+    let existing = {
+      number: 123,
+      title: "the title",
+      body: "the body",
+      base: {
+        ref: "base-ref",
+        repo: {
+          full_name: "base-org/reponame",
+          default_branch: "base-branch"
+        }
+      }
+    };
+
+    it("finds an existing pull request", () => {
+      let t = demoTransport.make({
+        git: {
+          originURL: "git@github.com:username/reponame.git",
+          currentBranch: "some-branch"
+        },
+        github: {
+          currentForkName: "username/reponame",
+          sourceForkName: "base-org/base-repo",
+          sourcePullRequests: [existing]
+        }
+      });
+
+      let pr, r = new Repository(".", t);
+
+      r.currentPullRequest((err, result) => pr = result);
+
+      waitsFor(() => pr);
+
+      runs(() => {
+        expect(pr.repository).toBe(r);
+        expect(pr.title).toBe("the title");
+        expect(pr.body).toBe("the body");
+        expect(pr.state).toBe(State.OPEN);
+
+        expect(pr.baseFork.github).toBe(t.github);
+        expect(pr.baseFork.fullName).toBe("base-org/base-repo");
+        expect(pr.baseFork.defaultBranch).toBe("master");
+        expect(pr.baseBranch).toBe("base-ref");
+
+        expect(pr.headFork.github).toBe(t.github);
+        expect(pr.headFork.fullName).toBe("username/reponame");
+        expect(pr.headBranch).toBe("some-branch");
+      });
+    });
+
+    it("creates a draft pull request");
 
   });
 
