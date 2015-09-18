fdf75e4e6c05dbe7e279ff8727faa57c0f8a96f3
Ash Wilson <smashwilson@gmail.com>
2015-09-18 14:29:02 -0400

    Test reference shortening.

--- spec/views/target-component-spec.js
+++ spec/views/target-component-spec.js
@@ -8,11 +8,11 @@ import PullRequest from '../../lib/models/pull-request';
 
 import demoTransport from '../../lib/transport/demo';
 
-describe("TargetComponent", function () {
+describe("TargetComponent", () => {
   let component, root;
   let repository, baseFork, headFork, pullRequest;
 
-  beforeEach(function () {
+  beforeEach(() => {
     repository = new Repository(".", demoTransport);
 
     baseFork = new Fork(repository, "base/repo", "master");
@@ -24,12 +24,23 @@ describe("TargetComponent", function () {
     root = component.element;
   });
 
-  it("shows the base fork and branch", function () {
+  it("shows the base fork and branch", () => {
     expect(root.querySelector("span.base").innerHTML).toEqual("base:base-branch");
   });
 
-  it("shows the head fork and branch", function () {
+  it("shows the head fork and branch", () => {
     expect(root.querySelector("span.head").innerHTML).toEqual("head:head-branch");
+  });
+
+  it("trims refs/heads/ from branch names", () => {
+    pullRequest.baseBranch = "refs/heads/base/branch";
+    pullRequest.headBranch = "refs/heads/head/branch";
+
+    component = new TargetComponent({pullRequest});
+    root = component.element;
+
+    expect(root.querySelector("span.base").innerHTML).toEqual("base:base/branch");
+    expect(root.querySelector("span.head").innerHTML).toEqual("head:head/branch");
   });
 
 });
